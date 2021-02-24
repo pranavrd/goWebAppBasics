a48ce85a4ed3bf6d5eefda7c39a35eb8bd00ae41
Pranav Doshetty <pranav.doshetty@gmail.com>
2021-02-24 10:05:11 +0530

    edit view

--- wiki.go
+++ wiki.go
@@ -32,10 +32,26 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 func main() {
 	p1 := &Page{Title: "t1", Body: []byte("Sample text.")}
 	p1.save()
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/view/", saveHandler)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 	p2, _ := loadPage("t1")
 	fmt.Println(string(p2.Body))
